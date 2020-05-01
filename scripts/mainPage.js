51c8d743fe7d98ef7855077fef8c0ebb8b63ef6f
Sam Merati <SamMerati@users.noreply.github.com>
2020-05-01 03:30:29 -0700

    Main Page content added buttons linked

---
+++ scripts/mainPage.js
@@ -0,0 +1,15 @@
+function game() {
+    location.replace('gameplay.html');
+  }
+  
+  function multiplayer() {
+    location.replace('multiplayer.html');
+  }
+  
+  function leaderboard() {
+    location.replace('leaderboard.html');
+  }
+  
+  function study() {
+    location.replace('study.html');
+  }
