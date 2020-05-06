9691172cb11c507a52703d7c9c5e4311db431a47
Ali <ali.nili128@gmail.com>
2020-05-05 21:06:40 -0700

    Refactored code.

--- game-board/run.js
+++ game-board/run.js
@@ -1,13 +1,32 @@
+// eslint-disable-next-line no-undef
 var Engine = Matter.Engine,
+  // eslint-disable-next-line no-undef
   Render = Matter.Render,
+  // eslint-disable-next-line no-undef
   Runner = Matter.Runner,
-  Composites = Matter.Composites,
+  // eslint-disable-next-line no-undef
   MouseConstraint = Matter.MouseConstraint,
+  // eslint-disable-next-line no-undef
   Mouse = Matter.Mouse,
+  // eslint-disable-next-line no-undef
   World = Matter.World,
+  // eslint-disable-next-line no-undef
   Bodies = Matter.Bodies;
 
+import {
+  createDot,
+  sizes,
+  colors,
+  addBorder,
+  makeInfected,
+  makeRecovered,
+  updateColorsBasedOnStatus,
+  updateStatusBasedOnDistance,
+  moveAllDotsRandomly,
+} from "./dots.js";
+
 const NUMBER_OF_DOTS = 10;
+const RENDER_PAUSE = 10;
 
 // create engine
 let engine = Engine.create();
@@ -34,23 +53,7 @@ Render.run(render);
 var runner = Runner.create();
 Runner.run(runner, engine);
 
-function createDot(color, radius) {
-  let dot = Bodies.circle(50, 50, radius || 10, {
-    friction: 0,
-    frictionAir: 0,
-    frictionStatic: 0,
-    force: {
-      x: (Math.random() / 500) * (Math.random() < 0.5 ? -1 : 1),
-      y: (Math.random() / 500) * (Math.random() < 0.5 ? -1 : 1),
-    },
-    render: {
-      fillStyle: color || "green",
-    },
-    label: "Uninfected",
-  });
-  return dot;
-}
-
+// add walls
 World.add(world, [
   Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
   Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
@@ -58,14 +61,10 @@ World.add(world, [
   Bodies.rectangle(0, 300, 50, 600, { isStatic: true }),
 ]);
 
-let userDot = createDot("Orange", 30);
-let kidDot = createDot("grey", 50);
-kidDot.isSensor = true;
-let anotherDot = createDot("black", 10);
-kidDot.render.opacity = 0.2;
-Matter.Body.setParts(userDot, [userDot, kidDot, anotherDot]);
+let userDot = createDot(sizes.USER_RADIUS, colors.USER);
+addBorder(userDot);
+
 World.add(world, userDot);
-// World.add(world, kidDot);
 
 // add mouse control
 var mouse = Mouse.create(render.canvas),
@@ -123,104 +122,105 @@ Render.lookAt(render, {
   max: { x: 800, y: 600 },
 });
 
-let isInfected = [];
-for (let i = 0; i < dots.length; i++) {
-  isInfected[i] = false;
-}
-isInfected[0] = true;
-for (let i = 0; i < dots.length; i++) {
-  if (isInfected[i]) {
-    dots[i].render.fillStyle = "red";
-    dots[i].label = "Infected";
-  }
-}
+makeInfected(dots[0]);
+updateColorsBasedOnStatus(dots);
 
 let firstTime = true;
 
 setTimeout(() => {
   setInterval(() => {
-    for (let i = 0; i < dots.length; i++) {
-      for (let j = 0; j < dots.length; j++) {
-        if (i === j) continue;
-        if (distance(dots[i], dots[j]) < 10000) {
-          if (isInfected[i]) {
-            isInfected[j] = true;
-          }
-          if (isInfected[j]) {
-            isInfected[i] = true;
-          }
-        }
-      }
-    }
-    for (let i = 0; i < dots.length; i++) {
-      if (isInfected[i]) {
-        dots[i].render.fillStyle = "red";
-        dots[i].label = "Infected";
-      }
-      dots[i].force = {
-        x: (Math.random() / 3000) * (Math.random() < 0.5 ? -1 : 1),
-        y: (Math.random() / 3000) * (Math.random() < 0.5 ? -1 : 1),
-      };
-    }
-    firstTime = false;
-  }, 0.5);
-}, 1000);
+    updateStatusBasedOnDistance(dots);
+    updateColorsBasedOnStatus(dots);
+    moveAllDotsRandomly(dots);
+  }, RENDER_PAUSE);
+}, 3000);
 
-function distance(dot1, dot2) {
-  return (
-    (dot1.position.x - dot2.position.x) * (dot1.position.x - dot2.position.x) +
-    (dot1.position.y - dot2.position.y) * (dot1.position.y - dot2.position.y)
-  );
-}
+// setTimeout(() => {
+//   setInterval(() => {
+//     for (let i = 0; i < dots.length; i++) {
+//       for (let j = 0; j < dots.length; j++) {
+//         if (i === j) continue;
+//         if (distance(dots[i], dots[j]) < 10000) {
+//           if (isInfected[i]) {
+//             isInfected[j] = true;
+//           }
+//           if (isInfected[j]) {
+//             isInfected[i] = true;
+//           }
+//         }
+//       }
+//     }
+//     for (let i = 0; i < dots.length; i++) {
+//       if (isInfected[i]) {
+//         dots[i].render.fillStyle = "red";
+//         dots[i].label = "Infected";
+//       }
+//       dots[i].force = {
+//         x: (Math.random() / 3000) * (Math.random() < 0.5 ? -1 : 1),
+//         y: (Math.random() / 3000) * (Math.random() < 0.5 ? -1 : 1),
+//       };
+//     }
+//     firstTime = false;
+//   }, 0.5);
+// }, 1000);
 
-Matter.Events.on(engine, "collisionActive", function (param) {
-  let arr = param.source.pairs.collisionActive;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].bodyA.id === kidDot.id || arr[i].bodyB.id === kidDot.id) {
-      if (
-        arr[i].bodyA.label === "Infected" ||
-        arr[i].bodyB.label === "Infected"
-      ) {
-        if (firstTime) break;
-        showLoseMessage();
-        break;
-      }
-    }
-  }
-});
+// }
 
-Matter.Events.on(engine, "collisionEnd", function (param) {
-  let arr = param.source.pairs.collisionActive;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].bodyA.id === kidDot.id || arr[i].bodyB.id === kidDot.id) {
-      if (
-        arr[i].bodyA.label === "Infected" ||
-        arr[i].bodyB.label === "Infected"
-      ) {
-        if (firstTime) break;
-        showLoseMessage();
-        break;
-      }
-    }
-  }
-});
+// Matter.Events.on(engine, "collisionActive", function (param) {
+//   let arr = param.source.pairs.collisionActive;
+//   // console.log(param.source.pairs);
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i].bodyA.id === kidDot.id || arr[i].bodyB.id === kidDot.id) {
+//       if (
+//         arr[i].bodyA.label === "Infected" ||
+//         arr[i].bodyB.label === "Infected"
+//       ) {
+//         // if (firstTime) break;
+//         showLoseMessage();
+//         break;
+//       }
+//     }
+//   }
+// });
 
-Matter.Events.on(engine, "collisionStart", function (param) {
-  let arr = param.source.pairs.collisionActive;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].bodyA.id === kidDot.id || arr[i].bodyB.id === kidDot.id) {
-      if (
-        arr[i].bodyA.label === "Infected" ||
-        arr[i].bodyB.label === "Infected"
-      ) {
-        if (firstTime) break;
-        showLoseMessage();
-        break;
-      }
-    }
-  }
-});
+// Matter.Events.on(engine, "collisionEnd", function (param) {
+//   let arr = param.source.pairs.collisionEnd;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i].bodyA.id === kidDot.id || arr[i].bodyB.id === kidDot.id) {
+//       if (
+//         arr[i].bodyA.label === "Infected" ||
+//         arr[i].bodyB.label === "Infected"
+//       ) {
+//         // if (firstTime) break;
+//         showLoseMessage();
+//         break;
+//       }
+//     }
+//   }
+// });
 
+// Matter.Events.on(engine, "collisionStart", function (param) {
+//   let arr = param.source.pairs.collisionStart;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i].bodyA.id === kidDot.id || arr[i].bodyB.id === kidDot.id) {
+//       if (
+//         arr[i].bodyA.label === "Infected" ||
+//         arr[i].bodyB.label === "Infected"
+//       ) {
+//         // if (firstTime) break;
+//         showLoseMessage();
+//         break;
+//       }
+//     }
+//   }
+// });
+
+let showMessage = true;
 function showLoseMessage() {
-  alert("You lost");
+  if (showMessage) {
+    alert("you lost");
+    // console.log("You lost");
+    // location.reload();
+  }
+  showMessage = false;
 }
