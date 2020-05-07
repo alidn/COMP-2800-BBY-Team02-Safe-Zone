c39cf595a0a249a1232b23f97bd953e29d6aca3a
Ali <ali.nili128@gmail.com>
2020-05-07 09:37:05 -0700

    added request handlers for login, logout, and signup

--- server/app.js
+++ server/app.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const { DatabaseClient } = require("../database/user");
+const WebSocket = require("ws");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
 app.use(require("./routes/user"));
+app.use(express.static(path.join(__dirname, "public")));
 
 new DatabaseClient().then((db) => {
   app.locals.databaseClient = db;
   console.log("connected to the database");
   app.listen(port, () => console.log(`app listening at port ${port}`));
+});
+
+const wss = new WebSocket.Server({ port: 8080, path: "/zas" });
+
+wss.on("connection", function connection(ws) {
+  ws.send("connected to the server");
+  ws.on("message", function incoming(message) {
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send("new message: " + message);
+      }
+    });
+  });
 });
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,54 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/user/:username', async (req, res) => {
-    let username = req.params.username; 
-    let db = req.app.locals.databaseClient; 
-    
-    let user = await db.getUserByUsername(username); 
-    res.json(user); 
+router.get("/api/user/:username", async (req, res) => {
+  let username = req.params.username;
+  let db = req.app.locals.databaseClient;
+
+  let user = await db.getUserByUsername(username);
+  res.json(user);
 });
 
-router.post('/user/create/');
+router.post("/login", async function (req, res) {
+  console.log(req.body);
+  console.log(req.query);
+  console.log(req.params);
+  let username = req.body.username;
+  let password = req.body.password;
+  let db = req.app.locals.databaseClient;
 
-module.exports = router; 
+  let { exists, error } = await db.usernameExists(username);
+
+  if (!exists) {
+    res.send({
+      loginSuccessful: false,
+      username: false,
+      password: null,
+      error: null,
+    });
+    return;
+  }
+
+  if (error !== null) {
+    res.send({
+      loginSuccessful: false,
+      username: null,
+      error: error,
+      password: null,
+    });
+    return;
+  }
+
+  let user = await db.getUserByUsername(username);
+  if (password !== user.password) {
+    res.send({
+      loginSuccessful: false,
+      username: true,
+      password: false,
+      error: null,
+    });
+  }
+
+  res.redirect("../public/index.html");
+});
+
+module.exports = router;
