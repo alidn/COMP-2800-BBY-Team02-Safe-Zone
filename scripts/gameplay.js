d41d17a3f3bd64f1936bacd6a4006b4717b7d7c2
bdiu <bdiu23@gmail.com>
2020-05-06 20:48:03 -0700

    added popup to play with others

--- scripts/gameplay.js
+++ scripts/gameplay.js
@@ -20,4 +20,56 @@ function restart(){
 
 function study(){
     location.replace('study.html');
+<<<<<<< Updated upstream
 }
+=======
+}
+
+let timer;
+let timeElapsed = 0;
+let timeTenth = 0;
+
+function tick() {
+
+    document.getElementById("score").innerHTML = "<h1>SCORE: " + timeElapsed + "." + timeTenth + " s</h1>";
+    timeTenth++;
+    if (timeTenth == 10) {
+        timeElapsed++;
+        timeTenth = 0;
+    }
+}
+
+function start() {
+    timer = setInterval(tick, 100);
+}
+
+function stop() {
+    clearInterval(timer);
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+  }
+}
+>>>>>>> Stashed changes
