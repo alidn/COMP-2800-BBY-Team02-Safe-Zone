a7f5000732f1906b91d899ce554de88562881617
partrima <iknubs@gmail.com>
2020-05-07 22:29:22 -0700

    created index.js and updated learderboard

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-
+const express = require("express");
 const MongoClient = require('mongodb').MongoClient;
+
+let app = express();
+    app.use(express.urlencoded({extended: true}));
+    app.use(express.static("HTML shell"));
+    app.set("view engine", "ejs");
+
+
 const uri = "mongodb+srv://zas:zastv@cluster0-vztfn.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
-  const collection = client.db("game").collection("users");
-  
-  client.close();
+  console.log(err);
 });
+
+
+app.get("leaderboard", (req, res) => {
+  const collection = client.db("game").collection("users");
+  res.render("views/leaderboard.ejs", collection);
+})
